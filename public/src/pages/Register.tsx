e1c549c0014312d71f4b03c10083543097c385b4
iskako01 <85966697+iskako01@users.noreply.github.com>
2023-02-05 19:50:57 +0100

    Added: registration form

--- public/src/pages/Register.tsx
+++ public/src/pages/Register.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </form>
+      </FormContainer>
+    </>
+  );
 };
+
+const FormContainer = styled.div``;
 
 export default Register;
