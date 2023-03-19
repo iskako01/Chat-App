38e679a26ba078057e006982d7831d4d59c24845
iskako01 <85966697+iskako01@users.noreply.github.com>
2023-03-19 12:18:35 +0100

    Added: message form

--- client/src/components/BaseMessageForm.tsx
+++ client/src/components/BaseMessageForm.tsx
@@ -1,4 +1,10 @@
 import { useState, FC } from "react";
+import {
+  PaperAirplaneIcon,
+  PaperClipIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+import Dropzone from "react-dropzone";
 
 interface PropsInterface {
   props: any;
@@ -10,15 +16,96 @@ const BaseMessageForm: FC<PropsInterface> = ({ props, activeChat }) => {
   const [attachment, setAttachment] = useState("");
   const [preview, setPreview] = useState("");
 
+  const cancelSendAttachment = () => {
+    setPreview("");
+    setAttachment("");
+  };
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const onSubmit = async () => {
+    setPreview("");
+
+    const date = new Date()
+      .toISOString()
+      .replace("T", "")
+      .replace("Z", `${Math.random() * 1000}+00:00`);
+
+    const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
+
+    console.log(props);
+    const form = {
+      attachments: at,
+      created: date,
+      sender_username: props.username,
+      text: message,
+      activeChatId: activeChat.id,
+    };
+
+    console.log(form);
+
+    props.onSubmit(form);
+    setMessage("");
+    setAttachment("");
+  };
+
   return (
     <div className="message-form-container">
       {preview && (
         <div className="message-form-preview ">
-          <img className="message-form-rev" src="" alt="" />
+          <img
+            className="message-form-preview-image"
+            src={preview}
+            alt="message-form-preview"
+            onLoad={() => URL.revokeObjectURL(preview)}
+          />
+          <XMarkIcon
+            className="message-form-icon-x"
+            onClick={cancelSendAttachment}
+          />
         </div>
       )}
+      <div className="message-form">
+        <div className="message-form-input-container">
+          <input
+            className="message-form-input"
+            type="text"
+            value={message}
+            onChange={handleChange}
+            placeholder="Send a message..."
+          />
+        </div>
+        <div className="message-form-icons">
+          <Dropzone
+            multiple={false}
+            noClick={true}
+            onDrop={(acceptedFiles) => {
+              setAttachment(acceptedFiles[0]);
+              setPreview(URL.createObjectURL(acceptedFiles[0]));
+            }}
+          >
+            {({ getRootProps, getInputProps, open }) => (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <PaperClipIcon
+                  className="message-form-icon-clip"
+                  onClick={open}
+                />
+              </div>
+            )}
+          </Dropzone>
+
+          <hr className="vertical-line" />
+          <PaperAirplaneIcon
+            className="message-form-icon-airplane"
+            onClick={onSubmit}
+          />
+        </div>
+      </div>
     </div>
   );
-}; 
+};
 
 export default BaseMessageForm;
